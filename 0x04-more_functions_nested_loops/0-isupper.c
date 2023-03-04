d0a4d85cf6e13e3fcc01044cef16526336dccd07
Maqxcs <maqzin5@gmail.com>
2023-03-04 11:06:16 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - uppercase letters
+ * @x:the number to be checked
+ * Return: 1 for upper letter or 0 for any else
+ */
+{
+	if (x >= 65 && x <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
